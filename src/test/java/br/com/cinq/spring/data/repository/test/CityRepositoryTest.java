5fd6647ade21b405fedf95f239423b9b73ccf29c
Willian Krause <krause.willian@gmail.com>
2017-07-13 10:41:38 -0300

    Repository tests adjustments for running.

--- src/test/java/br/com/cinq/spring/data/repository/test/CityRepositoryTest.java
+++ src/test/java/br/com/cinq/spring/data/repository/test/CityRepositoryTest.java
@@ -1,10 +1,19 @@
 package br.com.cinq.spring.data.repository.test;
 
+import br.com.cinq.spring.data.sample.application.Application;
+import br.com.cinq.spring.data.sample.entity.City;
+import br.com.cinq.spring.data.sample.entity.Country;
+import br.com.cinq.spring.data.sample.repository.CityRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 //import br.com.cinq.spring.data.sample.entity.City;
 //import br.com.cinq.spring.data.sample.entity.Country;
 //import br.com.cinq.spring.data.sample.repository.CityRepository;
@@ -15,23 +24,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @ActiveProfiles("unit")
 @DataJpaTest
+@SpringBootTest(classes = Application.class)
 public class CityRepositoryTest {
 
-//    @Autowired
-//    private CityRepository dao;
+    @Autowired
+    private CityRepository dao;
 
     @Test
     public void testQueryPerson() {
 
-//        Assert.assertNotNull(dao);
-        
-//        Assert.assertTrue(dao.count()>0);
+        Assert.assertNotNull(dao);
 
-//        Country country = new Country();
-//        country.setId(3); // Should be France
+        Assert.assertTrue(dao.count()>0);
 
-//        List<City> list = dao.findByCountry(country);
+        Country country = new Country();
+        country.setId(3L); // Should be France
 
-//        Assert.assertEquals(2, list.size());
+        List<City> list = dao.findByCountry(country);
+
+        Assert.assertEquals(2, list.size());
     }
 }
--- src/test/java/br/com/cinq/spring/data/repository/test/CountryRepositoryTest.java
+++ src/test/java/br/com/cinq/spring/data/repository/test/CountryRepositoryTest.java
@@ -1,43 +1,50 @@
 package br.com.cinq.spring.data.repository.test;
 
+import br.com.cinq.spring.data.sample.application.Application;
+import br.com.cinq.spring.data.sample.entity.Country;
+import br.com.cinq.spring.data.sample.repository.CountryRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import br.com.cinq.spring.data.sample.entity.Country;
-//import br.com.cinq.spring.data.sample.repository.CountryRepository;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @ActiveProfiles("unit")
 @DataJpaTest
+@SpringBootTest(classes = Application.class)
 public class CountryRepositoryTest {
 
-//    @Autowired
-//    private CountryRepository dao;
+    @Autowired
+    private CountryRepository dao;
 
     @Test
     public void testGelAllCountries() {
 
-//        Assert.assertNotNull(dao);
+        Assert.assertNotNull(dao);
 
-//        long count = dao.count();
+        long count = dao.count();
 
-//        Assert.assertTrue(count > 0);
+        Assert.assertTrue(count > 0);
 
-//        List<Country> countries = dao.findAll();
+        List<Country> countries = dao.findAll();
 
-//        Assert.assertEquals((int) count, countries.size());
+        Assert.assertEquals((int) count, countries.size());
     }
 
     @Test
     public void testFindOneCountry() {
 
-//        Assert.assertNotNull(dao);
+        Assert.assertNotNull(dao);
 
-//        List<Country> countries = dao.findLikeName("Fra");
+        List<Country> countries = dao.findByNameContaining("Fra");
 
-//        Assert.assertEquals(1, countries.size());
+        Assert.assertEquals(1, countries.size());
 
     }
 
